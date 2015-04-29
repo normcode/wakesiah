bf7a8e33f18fd14866e2d2d17b2aa6915cc48647
Scott Moynes <scott.moynes@gmail.com>
2015-04-28 22:10:21 -0700

    Add some silly tests.

--- test/wakesiah_test.exs
+++ test/wakesiah_test.exs
@@ -5,8 +5,21 @@ defmodule WakesiahTest do
     {:ok, pid} = Wakesiah.start_link name: __MODULE__
     {:ok, [pid: pid]}
   end
+
+  test "start_link with name option" do
+    {:ok, pid} = Wakesiah.start_link(name: :test)
+    assert Process.whereis(:test) == pid
+    assert {:links, [self]} == Process.info(pid, :links)
+  end
+
+  test "start_link without name option" do
+    assert {:ok, pid} = Wakesiah.start_link
+    assert {:links, [self]} == Process.info(pid, :links)
+  end
   
   test "membership list is empty on start", %{pid: pid} do
     assert Wakesiah.members(pid) == []
   end
+
+  
 end
