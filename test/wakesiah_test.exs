73418825fac3f26c21eadeaaef995f37a61a2fa7
Scott Moynes <scott.moynes@gmail.com>
2015-04-28 22:22:51 -0700

    Test Wakesiah.start without process linking.

--- test/wakesiah_test.exs
+++ test/wakesiah_test.exs
@@ -10,13 +10,28 @@ defmodule WakesiahTest do
     {:ok, pid} = Wakesiah.start_link(name: :test)
     assert Process.whereis(:test) == pid
     assert {:links, [self]} == Process.info(pid, :links)
+    Wakesiah.stop pid
   end
 
   test "start_link without name option" do
     assert {:ok, pid} = Wakesiah.start_link
     assert {:links, [self]} == Process.info(pid, :links)
+    Wakesiah.stop pid
   end
-  
+
+  test "start with name option" do
+    {:ok, pid} = Wakesiah.start(name: :test)
+    assert Process.whereis(:test) == pid
+    assert {:links, []} == Process.info(pid, :links)
+    Wakesiah.stop pid
+  end
+
+  test "start without name option" do
+    {:ok, pid} = Wakesiah.start
+    assert {:links, []} == Process.info(pid, :links)
+    Wakesiah.stop pid
+  end
+
   test "membership list is empty on start", %{pid: pid} do
     assert Wakesiah.members(pid) == []
   end
