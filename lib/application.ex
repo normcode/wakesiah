5401ef33f9f7d91e116226bbaf4cce40abcc10fa
Scott Moynes <scott.moynes@gmail.com>
2015-07-01 01:24:19 -0700

    Use parens on function call.

--- lib/application.ex
+++ lib/application.ex
@@ -3,7 +3,7 @@ defmodule WakesiahApp do
 
   def start(_type, _args) do
     worker_name = Application.get_env(:wakesiah, :registration, :wakesiah)
-    Wakesiah.Supervisor.start_link worker_name: worker_name
+    Wakesiah.Supervisor.start_link(worker_name: worker_name)
   end
 
 end
