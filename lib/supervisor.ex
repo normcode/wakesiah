e4954e644c61a3a829a5c18a08bc98d05d530103
Scott Moynes <scott.moynes@gmail.com>
2015-07-01 01:24:06 -0700

    Test Supervisor starts worker server

--- lib/supervisor.ex
+++ lib/supervisor.ex
@@ -1,16 +1,14 @@
 defmodule Wakesiah.Supervisor do
   use Supervisor
 
-  require Logger
-
-  @worker_name Wakesiah
+  @default_worker_name :wakesiah
 
   def start_link(opts \\ []) do
-    {worker_name, opts} = Keyword.pop(opts, :worker_name, @worker_name)
+    {worker_name, opts} = Keyword.pop(opts, :worker_name, @default_worker_name)
     Supervisor.start_link(__MODULE__, worker_name, opts)
   end
 
-  def init(worker_name, opts \\ []) do
+  def init(worker_name) do
     children = [
       worker(Wakesiah, [[name: worker_name]]),
     ]
---
+++ test/supervisor_test.exs
@@ -0,0 +1,24 @@
+defmodule SupervisorTest do
+  use ExUnit.Case, async: true
+
+  @test_worker_name __MODULE__
+
+  setup do
+    {:ok, pid} = Wakesiah.Supervisor.start_link(worker_name: @test_worker_name)
+
+    on_exit fn ->
+      Process.exit(pid, :normal)
+    end
+
+    {:ok, [pid: pid]}
+  end
+
+  test "supervisor starts worker", %{pid: sup_pid} do
+    worker_pid = Process.whereis(@test_worker_name)
+    children = Supervisor.which_children(sup_pid)
+
+    assert children == [{Wakesiah, worker_pid, :worker, [Wakesiah]}]
+    assert {:registered_name, @test_worker_name} == Process.info(worker_pid, :registered_name)
+  end
+
+end
