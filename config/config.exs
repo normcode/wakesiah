ef91787cc97d246549a6021e53ac29c83f0692b6
Scott Moynes <scott.moynes@gmail.com>
2015-09-06 11:36:02 -0700

    Only import environmental configuration if it exists.

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,6 @@ use Mix.Config
 config :logger, :console, level: :info,
   format: "$date $time [$level] $message\n"
 
-case Mix.env do
-  :test ->
-    import_config "#{Mix.env}.exs"
-  _ -> nil
+if Path.join(["config", "#{Mix.env}.exs"]) |> File.exists? do
+  import_config "#{Mix.env}.exs"
 end
